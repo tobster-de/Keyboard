2ae30024252500856893d01da6b17eeff527aeaf
Tobias Gärtner <tgaertner@gmx.net>
2016-03-23 16:02:25 +0100

    HACK: test changed shift behavior

--- KeyboardDemo/Program.cs
+++ KeyboardDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Keyboard;
@@ -10,22 +11,30 @@ namespace KeyboardDemo
 {
 	class Program
 	{
-		static void Main(string[] args)
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,
+            string lpWindowName);
+
+        static void Main(string[] args)
 		{
-			var p = new Process {StartInfo = {FileName = "notepad.exe"}};
+            //var p = new Process {StartInfo = {FileName = "notepad.exe"}};
+            //p.Start();
 
-			p.Start();
+            // Get a handle to the Calculator application. The window class
+            // and window name were obtained using the Spy++ tool.
+            IntPtr handle = FindWindow("CalcFrame", "Calculator");
 
-			try
+            try
 			{
-				List<Key> keys = "hello world".Select(c => new Key(c)).ToList();
-				var procId = p.Id;
-				Console.WriteLine("ID: " + procId);
+				List<Key> keys = "111*11=".Select(c => new Key(c)).ToList();
+				//var procId = p.Id;
+				//Console.WriteLine("ID: " + procId);
 				Console.WriteLine("Sending background keypresses to write \"hello world\"");
-				p.WaitForInputIdle();
+				//p.WaitForInputIdle();
 				foreach (var key in keys)
 				{
-					key.PressForeground(p.MainWindowHandle);
+				    key.PressForeground(handle); //p.MainWindowHandle);
 				}
 
 			}
